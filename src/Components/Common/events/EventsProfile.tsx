ecb45eadff8f6a36edddde01b09ea4d495dd6653
choiyen <yyy1459@naver.com>
2026-07-11 18:30:27 +0900

    문구 일부 수정

--- src/Components/Common/events/EventsProfile.tsx
+++ src/Components/Common/events/EventsProfile.tsx
@@ -46,24 +46,6 @@ const Eventdescriptions2 = styled.div`
   }
 `;
 
-const Eventdescriptions3 = styled.div`
-  border: 1px dotted rgba(0, 0, 0, 0.2);
-  padding: 16px 20px;
-  margin-top: 20px;
-  border-radius: 12px;
-  font-size: 1rem;
-  font-weight: 600;
-  color: #4d1040;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-  text-align: center;
-  line-height: 1.4;
-  text-align: left;
-
-  @media screen and (max-width: 600px) {
-    font-size: 0.7rem;
-  }
-`;
-
 const Emphasis1 = styled.span`
   color: #e91e63;
   font-weight: bold;
@@ -119,9 +101,8 @@ export const EventsProfile = ({ eventname }: Eventprops) => {
           이번에 홈페이지에 들어와 전화 또는 홈페이지 내 예약 서비스를 통해
           예약을 진행한 고객님들에 한하여,
           <CostFont>1만원</CostFont>을 <PlusFont>할인 </PlusFont> 해드립니다.{" "}
-          <br /> 신청이 많았으면
+          <br /> 신청이 많았으면 좋겠는데.....
         </Eventdescriptions2>
-        <Eventdescriptions3>좋겠는데.....</Eventdescriptions3>
       </EventContainer>
     </div>
   );
